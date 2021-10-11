7104cb5ebf21df922d4cfaea9e7befc1b54a1e70
aladdinjoy233 <ninjalover249@gmail.com>
2021-10-11 18:23:51 -0300

    Add UI to the RPS game, also moved styles to independant CSS file

--- main.js
+++ main.js
@@ -1,41 +1,86 @@
 const gameOptions = ["rock", "paper", "scissors"];
+const siteChoices = document.querySelectorAll(".choice");
+const roundWinnerDiv = document.querySelector(".announce-round-winner");
+const matchWinnerOverlay = document.querySelector(".match-finished-overlay");
+const matchWinnerTitle = document.querySelector(".winner-title");
+const restartBtn = document.querySelector("#restart-btn")
 
-// Ask player for the input
-function getPlayerPlay() {
-  let askPlayer = prompt("Rock, Paper, or Scissors?");
-  return askPlayer.toLowerCase();
-}
+let playerScoreSpan = document.querySelector(".player-score");
+let compScoreSpan = document.querySelector(".computer-score");
 
-// Get random number between MIN and MAX
+let playerScore = 0;
+let computerScore = 0;
+
+// Get a random number between the MIN number and the MAX number
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Get computer's play
+// Get computer's play utilzizing the random number function
 function computerPlay() {
-  let currentChoice = gameOptions[getRandomInt(0, 2)];
-  return currentChoice;
+  return gameOptions[getRandomInt(0, 2)];
 }
 
-// Play one round of the game
+// Function that does everything it should do when a player wins a round
+function playerWin(playerSelection, computerSelection) {
+  roundWinnerDiv.textContent = `You win, ${playerSelection} beats ${computerSelection}!`;
+  playerScore++;
+  playerScoreSpan.textContent = playerScore;
+}
+
+// Function that does everything it should do when a player loses a round (PC WINS)
+function compWin(playerSelection, computerSelection) {
+  roundWinnerDiv.textContent = `You loose, ${computerSelection} beats ${playerSelection}!`
+  computerScore++
+  compScoreSpan.textContent = computerScore;
+}
+
+// Play a single round of the game
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) return "It's a tie!";
+  if (playerSelection === computerSelection) roundWinnerDiv.textContent = "It's a tie!";
 
   // Loosing
   if (
-      playerSelection === "rock" && computerSelection === "paper" ||
-      playerSelection === "paper" && computerSelection === "scissors" ||
-      playerSelection === "scissors" && computerSelection === "rock") {
-        return `You loose, ${computerSelection} beats ${playerSelection}!`
+    playerSelection === "rock" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "rock") {
+    compWin(playerSelection, computerSelection);
   } else {
     // Winning
-    return `You win, ${playerSelection} beats ${computerSelection}!`;
+    playerWin(playerSelection, computerSelection);
   };
 }
 
-// Play the game 5 times
-function game() {
-  for (i = 0; i < 5; i++){
-    console.log(playRound(getPlayerPlay(), computerPlay()));
+// Check for a winner (When there are 5 points for any player)
+function checkWinner() {
+  if (playerScore >= 5 || computerScore >= 5) {
+    let winner = playerScore >= 5 ? "player" : "computer";
+
+    if (winner === "player") {
+      matchWinnerTitle.innerText = "Congratulations! You Win!";
+    } else {
+      matchWinnerTitle.innerText = "Maybe you'll win next time. . .";
+    }
+
+    matchWinnerOverlay.classList.add("game-over");
   }
 }
+
+// Listen for click on any of the three choice buttons
+siteChoices.forEach(choice => {
+  choice.addEventListener("click", (e) => {
+    let playersChoice = gameOptions[choice.getAttribute('data-num-in-array')];
+    playRound(playersChoice, computerPlay());
+    checkWinner();
+  })
+})
+
+// Restart the game when the button is clicked
+restartBtn.addEventListener("click", () => {
+  playerScore = 0;
+  playerScoreSpan.textContent = playerScore;
+  computerScore = 0;
+  compScoreSpan.textContent = computerScore;
+  roundWinnerDiv.textContent = "Winner will be announced here";
+  matchWinnerOverlay.classList.remove("game-over");
+})
